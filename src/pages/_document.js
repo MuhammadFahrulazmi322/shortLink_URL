0100637f2d729f562e5827e84c36974ad56c0ba8
MuhammadFahrulazmi322 <fazmi322@gmail.com>
2024-04-02 11:49:38 +0700

    update title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head title="Shorten URL" />
+      <Head>
+        <title>Shorten URL</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
